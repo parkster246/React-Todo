b9b198bf9d426f14e605c483f2aaca6b5f0ee4b9
Parker williams <parkster246@gmail.com>
2020-08-03 15:05:17 -0500

    stretch done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TodoList from './components/TodoList.js'
 import TodoForm from './components/TodoForm.js'
+import SearchBar from './components/SearchBar.js'
 import './App.css'
 function addTask(obj, task) {
   obj.todo.push({
@@ -8,6 +9,11 @@ function addTask(obj, task) {
     id: Date.now(),
     completed: false,
   })
+  obj.searched.push({
+    task: task,
+    id: Date.now(),
+    completed: false,
+  });
   return obj;
 }
 
@@ -17,14 +23,31 @@ function editTask(obj, id) {
       obj.todo[i].completed = !obj.todo[i].completed
     }
   }
+  for (let i=0;i<obj.searched.length;i++) {
+    if (obj.searched[i].id === id) {
+      obj.searched[i].completed = !obj.searched[i].completed
+    }
+  }
   return obj;
 }
 
 function handleClearing(obj) {
   const filtered = obj.todo.filter(item => item.completed === false)
-  console.log(filtered)
   obj.todo = filtered;
+  obj.searched = filtered;
   return obj;
+}
+function searching(obj, string) {
+  if (string.length > 0) {
+    const searched_array = obj.todo.filter( item => { return (item.task.toUpperCase().indexOf(string.toUpperCase()) > -1)});
+    obj.searched = searched_array;
+    return obj
+  }
+  else if (string.length === 0) {
+    console.log("empty")
+    obj.searched = obj.todo;
+    return obj
+  }
 }
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -33,13 +56,14 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      todo : []
+      todo : [],
+      searched: []
       
     }
   }
   componentDidMount() {
     if (localStorage.getItem("todo")) {
-      this.setState({todo : JSON.parse(localStorage.getItem("todo"))});
+      this.setState({todo : JSON.parse(localStorage.getItem("todo")), searched: JSON.parse(localStorage.getItem("todo")) });
     } else {
       localStorage.setItem("todo", JSON.stringify([]));
     }
@@ -60,11 +84,16 @@ class App extends React.Component {
     this.setState(handleClearing(this.state));
     console.log(this.state)
   }
+  handleSearchBar = (string) => {
+    this.setState(searching(this.state, string), () => {});
+
+  }
   render() {
     return (
       <div className='app'>
         <h2>The Todo List</h2>
-        <TodoList todoChange={this.todoChange} list={this.state.todo}/>
+        <SearchBar handleSearchBar={this.handleSearchBar}/>
+        <TodoList todoChange={this.todoChange} list={this.state.searched}/>
         <TodoForm handleClear={this.handleClear} onAdd={this.handleAdd}/>
       </div>
     );
