a2d28b3ecca6eb3599e26df727cd918764578372
Parker williams <parkster246@gmail.com>
2020-08-03 14:33:53 -0500

    multiple changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import React from 'react';
 import TodoList from './components/TodoList.js'
+import TodoForm from './components/TodoForm.js'
+import './App.css'
+function addTask(obj, task) {
+  obj.todo.push({
+    task: task,
+    id: Date.now(),
+    completed: false,
+  })
+  return obj;
+}
 
+function editTask(obj, id) {
+  for (let i=0;i<obj.todo.length;i++) {
+    if (obj.todo[i].id === id) {
+      obj.todo[i].completed = !obj.todo[i].completed
+    }
+  }
+  return obj;
+}
+
+function handleClearing(obj) {
+  const filtered = obj.todo.filter(item => item.completed === false)
+  console.log(filtered)
+  obj.todo = filtered;
+  return obj;
+}
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
@@ -22,11 +47,24 @@ class App extends React.Component {
       ]
     }
   }
+  handleAdd = task => {
+    this.setState(addTask(this.state, task));
+  };
+
+  todoChange = (id) => {
+    this.setState(editTask(this.state, id));
+  }
+
+  handleClear = () => {
+    this.setState(handleClearing(this.state));
+    console.log(this.state)
+  }
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
-        <TodoList list = {this.state.todo}/>
+        <TodoList todoChange={this.todoChange} list={this.state.todo}/>
+        <TodoForm handleClear={this.handleClear} onAdd={this.handleAdd}/>
       </div>
     );
   }
